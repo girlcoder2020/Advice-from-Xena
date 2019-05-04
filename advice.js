90e6fb02674ba66be3f535d935eb42b54ca2b7a0
Amanda <48845910+girlcoder2020@users.noreply.github.com>
2019-05-04 15:48:55 -0500

    added code files
    added HTML, CSS, JS codes initially created in CodePen and saved in VS Code

---
+++ advice.js
@@ -0,0 +1,23 @@
+fill(204, 156, 74);
+rect(60, 275, 298, 57, 14);
+let answer = floor(random(0, 5));
+if (answer === 0) {
+    fill(8, 8, 8);
+    text("You have many skills...", 155, 308);
+}
+    else if (answer === 1){
+    fill(8, 8, 8);
+    text("Well, son of a Bacchae!", 145, 308); 
+}
+    else if (answer === 2){
+        fill(8, 8, 8);
+    text("You gotta be flexible, it comes with the territory", 88, 308); 
+}
+    else if (answer === 3){
+        fill(8, 8, 8);
+    text("Well don't knock it til ya tried it", 132, 308); 
+}
+    else if (answer === 4){
+        fill(8, 8, 8);
+    text("If that's the worst that happens today I'll be thrilled", 76, 308); 
+}
